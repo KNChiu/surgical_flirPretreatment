8b635286949c6cc09741672b10c6f2b903db90a8
KNChiu <y9760210@gmail.com>
2021-11-01 16:56:32 +0800

    加入直方圖分析方法

--- flirReadpreprocs.py
+++ flirReadpreprocs.py
@@ -7,6 +7,9 @@ import numpy as np
 import os 
 import cv2
 
+drawHist = False                                # 畫出直方圖
+drawMask = False                                # 劃出遮罩
+
 minTemp = maxTemp = None                        # 溫度上下限
 palettes = [cm.gnuplot2]                        # 影像調色板
 
@@ -20,7 +23,7 @@ for path, dir_list, file_list in imgPath:
 
         flir = flirimageextractor.FlirImageExtractor(palettes=palettes)                             # 熱影像轉換套件
         # print(hotPath)
-        flir.process_image(hotPath)                                         
+        flir.process_image(hotPath)       
         flirRGB = flir.extract_embedded_image()                                                     # 輸出 RGB
         flirHot = flir.get_thermal_np()                                                             # 輸出 1/2 大小熱影像資訊
 
@@ -31,39 +34,55 @@ for path, dir_list, file_list in imgPath:
         print("MAX Thermal :", np.amax(flirHot))
         print("MIN Thermal :", np.amin(flirHot))
 
-
-        # print("MAX Normal :", np.amax(thermal_normalized))
-        # print("Min Normal :", np.amin(thermal_normalized))
-
-
-        # print("MAX AutoNormal :", np.amax(autoNormal))
-        # print("Min AutoNormal :", np.amin(autoNormal))
-
+        
         flirMean = flirHot.mean()                                                                 # 計算整張熱影像平均
         ret, flimask = cv2.threshold(flirHot, flirMean, 255, cv2.THRESH_BINARY)                   # 產生遮罩
 
         autoNormal[flimask < 255] = 0                                                             # 二質化
 
-        fig = plt.figure()
-        subplot1=fig.add_subplot(1, 4, 1)
-        subplot1.imshow(flirRGB)
-        subplot1.set_title("RGB image")
 
-        subplot2=fig.add_subplot(1, 4, 2)
-        subplot2.imshow(autoNormal, cmap=cm.gnuplot2)
-        subplot2.set_title("Flir image")
+        if drawHist:
+            print('mean :', flirMean)
+            plt.title("Thermal Distribution")
+            plt.xlabel("Thermal")
+            plt.ylabel("Value")
 
-        subplot3=fig.add_subplot(1, 4, 3)
-        subplot3.imshow(flimask)
-        subplot3.set_title("Thresh Mask")
+            plt.text(31.45, 477.5, "Mean : " + str(round(flirMean, 2)),
+                    fontsize=15,
+                    color="red",
+                    verticalalignment ='top', 
+                    horizontalalignment ='center',
+                    bbox ={'facecolor':'white', 
+                            'pad':10}
+            )
 
-        subplot4=fig.add_subplot(1, 4, 4)
-        subplot4.imshow(autoNormal, cmap=cm.gnuplot2)
-        subplot4.set_title("Flir Matting")
+            plt.hist(flirHot)
+            plt.xlim([15, 35])
+            plt.ylim([0, 500])
+            plt.vlines(flirMean, 1, 400, color="red")                          
+            plt.show()
+        
+        if drawMask:
+            fig = plt.figure()
+            subplot1=fig.add_subplot(1, 4, 1)
+            subplot1.imshow(flirRGB)
+            subplot1.set_title("RGB image")
 
-        # fig.suptitle("Flir Image")
-        fig.tight_layout()
-        fig.savefig(pltSavepath, dpi=1000, bbox_inches='tight')
-        plt.close('all')
+            subplot2=fig.add_subplot(1, 4, 2)
+            subplot2.imshow(autoNormal, cmap=cm.gnuplot2)
+            subplot2.set_title("Flir image")
+
+            subplot3=fig.add_subplot(1, 4, 3)
+            subplot3.imshow(flimask)
+            subplot3.set_title("Thresh Mask")
+
+            subplot4=fig.add_subplot(1, 4, 4)
+            subplot4.imshow(autoNormal, cmap=cm.gnuplot2)
+            subplot4.set_title("Flir Matting")
+
+            # fig.suptitle("Flir Image")
+            fig.tight_layout()
+            fig.savefig(pltSavepath, dpi=1000, bbox_inches='tight')
+            plt.close('all')
 
 
