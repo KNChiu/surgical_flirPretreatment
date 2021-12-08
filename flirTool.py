861c85edd57e6a3c88fa4e582ca4305711b99c30
KNChiu <y9760210@gmail.com>
2021-12-08 13:25:58 +0800

    圈出N度溫差範圍

--- flirTool.py
+++ flirTool.py
@@ -17,7 +17,7 @@ minTemp = maxTemp = None                        # 溫度上下限
 palettes = [cm.gnuplot2]                        # 影像調色板
 
 imgPath = os.walk(r'sample\\all_information')   # 輸入路徑
-savePath = r'sample\\plt_save'
+savePath = r'sample\\frame_save'
 
 
 class FlirPretreatment():
@@ -27,7 +27,7 @@ class FlirPretreatment():
         self.palettes = palettes
         self.imgPathlist = self.getImglist()
 
-    def getImglist(self):
+    def getImglist(self):                                                           # 遍歷路徑
         imgPathlist = []
         for path, dir_list, file_list in self.imgPath: 
             for file_name in file_list:
@@ -35,7 +35,7 @@ class FlirPretreatment():
         
         return imgPathlist
 
-    def drawHist(self, flirHot, flirMean):
+    def drawHist(self, flirHot, flirMean):                                          # 畫出溫度直線圖
         print('mean :', flirMean)
         plt.title("Thermal Distribution")
         plt.xlabel("Thermal")
@@ -57,7 +57,7 @@ class FlirPretreatment():
         plt.show()
         plt.close('all')
 
-    def drawMask(self, flirRGB, flirHot, flimask, normalObject, pltSavepath):
+    def drawMask(self, flirRGB, flirHot, flimask, normalObject, pltSavepath):       # 畫出患者範圍
         fig = plt.figure()
         subplot1=fig.add_subplot(1, 4, 1)
         subplot1.imshow(flirRGB)
@@ -75,7 +75,8 @@ class FlirPretreatment():
         subplot4.imshow(normalObject, cmap=cm.gnuplot2)
         subplot4.set_title("Flir Matting")
 
-        # fig.suptitle("Flir Image")
+        # figTitle = "MAX Thermal :"+ str(round(np.amax(flirHot), 2))+ "  |  " + "MIN Thermal :"+ str(round(np.amin(flirHot), 2))
+        # fig.suptitle("")
         fig.tight_layout()
 
         if pltSavepath:
@@ -83,7 +84,39 @@ class FlirPretreatment():
             plt.close('all')
         plt.show()
 
-    def draw3D(self, flirObject, hotObject, flirHot, pltSavepath):
+    def drawFrame(self, flirRGB, flirHot, flimask, thermalRange, pltSavepath):      # 圈出溫差範圍
+        flirframe = flirHot.copy()
+        flirframe[flirframe < np.amax(flirHot) - thermalRange] = 0         
+        flirframe[flirframe >= np.amax(flirHot) - thermalRange] = 255
+
+
+        fig = plt.figure()
+        subplot1=fig.add_subplot(1, 4, 1)
+        subplot1.imshow(flirRGB)
+        subplot1.set_title("RGB image")
+
+        subplot2=fig.add_subplot(1, 4, 2)
+        subplot2.imshow(flirHot, cmap=cm.gnuplot2)
+        subplot2.set_title("Flir image")
+
+        subplot3=fig.add_subplot(1, 4, 3)
+        subplot3.imshow(flimask)
+        subplot3.set_title("Thresh Mask")
+
+        subplot4=fig.add_subplot(1, 4, 4)
+        subplot4.imshow(flirframe, cmap=cm.gnuplot2)
+        subplot4.set_title("Flir Frame")
+
+        figTitle = "MAX Thermal :"+ str(round(np.amax(flirHot), 2))+ "  |  " + "MIN Thermal :"+ str(round(np.amin(flirHot), 2))
+        fig.suptitle(figTitle)
+        fig.tight_layout()
+
+        if pltSavepath:
+            fig.savefig(pltSavepath, dpi=1000, bbox_inches='tight')
+            plt.close('all')
+        plt.show()
+
+    def draw3D(self, flirObject, hotObject, flirHot, pltSavepath):                  # 畫出地形圖
         x_size = flirObject.shape[1]
         y_size =  flirObject.shape[0]
         x = np.linspace(0, x_size-1, x_size)
@@ -110,7 +143,7 @@ class FlirPretreatment():
         ax.set_xlabel('image_X')
         ax.set_ylabel('image_Y')
         ax.set_zlabel('Thermal')
-        ax.plot_surface(X, Y, flirHot, rstride=1,cstride=1,cmap=plt.cm.gnuplot2)                         # 生成一個曲面
+        ax.plot_surface(X, Y, flirHot, rstride=1,cstride=1,cmap=plt.cm.gnuplot2)                  # 生成一個曲面
         # fig.tight_layout()
         if pltSavepath:
             fig.savefig(pltSavepath, dpi=1000, bbox_inches='tight')
@@ -118,12 +151,11 @@ class FlirPretreatment():
         plt.show()
 
     def separateNP(self, imgPath):
-        flir = flirimageextractor.FlirImageExtractor(palettes=self.palettes)                             # 熱影像轉換套件
+        flir = flirimageextractor.FlirImageExtractor(palettes=self.palettes)                       # 熱影像轉換套件
         flir.process_image(imgPath)       
         flirRGB = flir.extract_embedded_image()                                                     # 輸出 RGB
         flirHot = flir.get_thermal_np()                                                             # 輸出 1/2 大小熱影像資訊
         
-
         return flirRGB, flirHot
     
     def makeMask(self, flirHot, autoNormal):
@@ -145,16 +177,18 @@ class FlirPretreatment():
             flirRGB, flirHot = self.separateNP(imgPath)                                             # 分離原始圖像與溫度影像
 
             autoNormal = (flirHot - np.amin(flirHot)) / (np.amax(flirHot) - np.amin(flirHot))       # 標準化到 0~1 之間
+            flimask, normalObject, hotObject = self.makeMask(flirHot, autoNormal)                   # 建立遮罩影像
+            flirMean = flirHot.mean() 
 
+            print("===========================")
+            print(imgPath)
             print("MAX Thermal :", np.amax(flirHot))
             print("MIN Thermal :", np.amin(flirHot))
 
-            flimask, normalObject, hotObject = self.makeMask(flirHot, autoNormal)                   # 建立遮罩影像
-            
-
-            #     self.drawHist(flirHot, flirMean)
-            self.drawMask(flirRGB, flirHot, flimask, normalObject, pltSavepath=None)
-            self.draw3D(normalObject, hotObject, flirHot, pltSavepath=None)
+            # self.drawHist(flirHot, flirMean)
+            # self.drawMask(flirRGB, flirHot, flimask, normalObject, pltSavepath=None)
+            self.drawFrame(flirRGB, flirHot, flimask, thermalRange = 4, pltSavepath=savePath)       # 圈出溫差 N度內範圍 
+            # self.draw3D(normalObject, hotObject, flirHot, pltSavepath=None)
 
 
 if __name__ == '__main__':
