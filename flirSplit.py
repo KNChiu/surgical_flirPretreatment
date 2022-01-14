3348634012b1143696dbb167ac8102d5eadcfef7
KNChiu <y9760210@gmail.com>
2022-01-14 11:27:33 +0800

    修正plt存圖邊框問題

--- flirSplit.py
+++ flirSplit.py
@@ -15,33 +15,54 @@ logging.basicConfig(level=logging.ERROR)          # 避免出現WARNINIG
 
 
 class flir_img_split:
-    def __init__(self, imgPath):
+    def __init__(self, imgPath, palettes, savePath):
         self.imgPath = imgPath
+        self.savePath = savePath
+        self.palettes = palettes
         pass
 
-    def getImglist(self):                                                           # 遍歷路徑
+    def getImglist(self):                           # 取得每個影像路徑
         imgPathlist = []
-        for path, dir_list, file_list in self.imgPath: 
+        for path, dir_list, file_list in self.imgPath:                               
             for file_name in file_list:
-                if file_name.split(".")[-1] == "jpg":
+                if file_name.split(".")[-1] == "jpg":                                               # 只保留副檔名為*.jpg的影像
                     imgPathlist.append(os.path.join(str(path), str(file_name)))
-        
+                
         return imgPathlist
 
     def separateNP(self, imgPath):     # 分離原始圖像與溫度影像
-        flir = flirimageextractor.FlirImageExtractor(palettes=self.palettes)                       # 熱影像轉換套件
+        flir = flirimageextractor.FlirImageExtractor(palettes=self.palettes)                        # 熱影像轉換套件
         flir.process_image(imgPath)       
         flirRGB = flir.extract_embedded_image()                                                     # 輸出 RGB
         flirHot = flir.get_thermal_np()                                                             # 輸出熱影像資訊
         
         return flirRGB, flirHot
 
-
+    def saveCmap(self, flirHot, pltSavepath):
+        plt.imshow(flirHot, cmap=cm.gnuplot2)
+        plt.axis('off')
+        if pltSavepath:
+            print("save at:"+ str(pltSavepath))
+            plt.imsave(pltSavepath, flirHot, cmap=cm.gnuplot2)
+            plt.close('all')
+        plt.show()
 
 
 if __name__ == '__main__':
-    imgPath = os.walk(r'G:\我的雲端硬碟\Lab\Project\外科溫度\醫師分享圖片\Ischemia FLIR')   # 輸入路徑
+    imgInputpath = os.walk(r'G:\我的雲端硬碟\Lab\Project\外科溫度\醫師分享圖片\Ischemia FLIR')   # 輸入路徑
+    savePath = r'splitImg\flir'
+    palettes = [cm.gnuplot2]                        # 影像調色板
 
-    flirSplit = flir_img_split(imgPath)
-    flirSplit.getImglist()
+    flirSplit = flir_img_split(imgInputpath, savePath, palettes)
+    for imgPath in flirSplit.getImglist():
+        print(imgPath)
+        flirRGB, flirHot = flirSplit.separateNP(imgPath)
+        print(np.shape(flirRGB), np.shape(flirHot))
+
+        imgName = os.path.split(imgPath)[-1]
+        savePath = os.path.join(savePath, imgName)
+
+        flirSplit.saveCmap(flirHot, savePath)
+        break
+        
 
